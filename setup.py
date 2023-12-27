8adb9283ad5273c270a30a7a1768cb57ec11d724
Akshay Wadhavane <akshay.wadhavane@hotmail.com>
2023-12-27 12:37:47 +1100

    install sf fuel sdk separately

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     url='https://github.com/seb-angel/FuelSDK-Python-Wrapper',
     license='MIT',
     install_requires=[
-        'Salesforce-FuelSDK>=1.3.0',
         'PyJWT>=0.1.9',
         'requests>=2.18.4',
         'suds-py3>=1.4'
